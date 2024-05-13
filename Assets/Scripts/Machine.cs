cc74a97d4ff3bf0bc0c6cf3477ac8d01cd4e6105
Aayush Gupta <aayush.gupta555@gmail.com>
2024-05-14 02:18:05 +0530

    Final Edit

--- Assets/Scripts/Machine.cs
+++ Assets/Scripts/Machine.cs
@@ -8,22 +8,19 @@ using UnityEngine;
 public class Machine : BezierWalkerWithSpeed
 {
     [SerializeField] private IceCream_List_Scriptable iceCream_List_Scriptable;
-    [SerializeField] private Transform iceCreamCone;
-    [SerializeField] private Transform creamFilter;
-    [SerializeField] [Range(1,10)] private float durationOfFallingPiece;
+    [SerializeField] private Transform @base;
+    [SerializeField] private Transform spawnPoint;
+    [SerializeField] [Range(1,10)] private float duration;
     private bool machineIsRunning ;
     private bool iceCreamFinished;
     private float initialHeight;
-    private GameObject iceCreamPrefab;
+    private GameObject iceCream_Prefab;
     private GameObject iceCreamGO;
-
-
 
     void Start()
     {
         initialHeight = transform.position.y;
     }
-
 
     protected override void Update()
     {
@@ -42,33 +39,32 @@ public class Machine : BezierWalkerWithSpeed
 
     private void FixedUpdate()
     {
-        if(iceCreamPrefab != null && machineIsRunning && !iceCreamFinished)
+        if(iceCream_Prefab != null && machineIsRunning && !iceCreamFinished)
         {
-            iceCreamGO = Instantiate(iceCreamPrefab, creamFilter.position, transform.rotation);
+            iceCreamGO = Instantiate(iceCream_Prefab, spawnPoint.position, transform.rotation);
 
-            iceCreamGO.transform.DOMove((spline.GetPoint(NormalizedT)), durationOfFallingPiece);
+            iceCreamGO.transform.DOMove((spline.GetPoint(NormalizedT)), duration);
 
-            iceCreamGO.transform.SetParent(iceCreamCone);
+            iceCreamGO.transform.SetParent(@base);
             
         }
     }
 
-
     public void OnPathComplete(){
         iceCreamFinished = true;
-        iceCreamPrefab = null;
+        iceCream_Prefab = null;
         machineIsRunning = false;
     }
 
     public void OnMachineStart(IceCream_Enum type)
     {
         IceCream_Asset asset = iceCream_List_Scriptable.iceCream_List.Find(item => item.iceCream_Type == type.iceCream_Type);
-        iceCreamPrefab = asset.iceCream_Prefab;  
+        iceCream_Prefab = asset.iceCream_Prefab;  
         machineIsRunning = true;
     }
 
     public void OnMachineStop(){
-        iceCreamPrefab = null;
+        iceCream_Prefab = null;
         machineIsRunning = false;
     }
 
