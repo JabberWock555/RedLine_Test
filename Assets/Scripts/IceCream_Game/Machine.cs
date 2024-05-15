a7dff74535d9884c36f83c2f08ba862523b2883b
Aayush Gupta <aayush.gupta555@gmail.com>
2024-05-16 02:01:25 +0530

    Jelly Dye Game

--- Assets/Scripts/IceCream_Game/Machine.cs
+++ Assets/Scripts/IceCream_Game/Machine.cs
@@ -1,7 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using BezierSolution;
-using BezierSolution.Extras;
 using DG.Tweening;
 using UnityEngine;
 
---
+++ Assets/Scripts/JellyDye_Game/ColoringObject.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+
+
+public class ColoringObject : MonoBehaviour, IColoringObject
+{
+    [SerializeField] private MeshRenderer _renderer;
+    [SerializeField] private Material _coloringMaterial;
+    [SerializeField] private Color _color;
+    [SerializeField] private float _coloringSpeed = 1;
+    private Material _coloringMaterialInstance;
+    private float _fillAmount;
+    private Vector2 _paintPoint; 
+    private bool _painting;
+    private bool _paintingFinished;
+
+    private void Awake()
+    {
+        _coloringMaterialInstance = Instantiate(_coloringMaterial);
+        _renderer.material = _coloringMaterialInstance; 
+        ResetColoring();
+    }
+
+    private void Start()
+    {
+        Assign();   
+    }
+
+    private void Update()
+    {
+        if (_painting && _fillAmount <= 1)
+        {
+            OnColoring();
+        }
+    }
+    
+    public void Assign()
+    {
+       ColoringManager.Instance.AddColoringObject(this);
+    }
+    
+    public void ResetColoring()
+    {
+        _fillAmount = 0;
+        _coloringMaterialInstance.SetFloat("_FillAmount", _fillAmount);
+        _painting = false;
+        _paintingFinished = false;
+    }    
+    
+    private void OnColoring()
+    {
+        _fillAmount += Time.deltaTime * .01f * _coloringSpeed;
+        if (_fillAmount > 1)
+        {
+            _painting = false;
+            _paintingFinished = true;
+        }
+        _coloringMaterialInstance.SetFloat("_FillAmount",_fillAmount);
+    }
+
+    public void StartColoring(Vector2 position, Color color)
+    {
+        if (_painting || _paintingFinished)
+        {
+            return;
+        }
+        _color = color;
+        _coloringMaterialInstance.SetColor("_TextureColor",_color);
+        _coloringMaterialInstance.SetVector("_TouchPosition",Vector2.one*.5f-position);
+        _painting = true;
+    }
+}
+
+public interface IColoringObject
+{
+    void Assign();
+    void ResetColoring();
+    void StartColoring(Vector2 position, Color color);
+}
